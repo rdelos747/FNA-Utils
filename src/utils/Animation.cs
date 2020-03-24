ee2db7872f3a40ec6dfa21c33a13602e770e5d8b
rafael de los santos <rdelos747@gmail.com>
2020-03-24 17:30:50 -0400

    Spritesheet class (#7)
    * create sprite sheet helper
    
    * add color and node bound draw depth
    
    * add keyframes class
    
    * update readme for animations

--- src/utils/Animation.cs
+++ src/utils/Animation.cs
@@ -3,109 +3,57 @@ using Microsoft.Xna.Framework;
 
 namespace Utils {
 
-  public enum AnimationType {
-    CURVE,
-    STEP
-  }
-
   public class Animation {
+    public int Index { get; private set; }
+    public bool Loop = true;
+    private int ElapsedTime = 0;
+    private KeyFrames Frames;
 
-    int elapsedTime = 0;
-    int maxTime = 0;
-    int numPoints = 0;
-    public int index { get; private set; }
-    public bool loop = true;
-    public AnimationType animationType;
-
-    Curve curve = new Curve();
-
-    public Animation(bool setLoop = true, AnimationType type = AnimationType.CURVE) {
-      loop = setLoop;
-      animationType = type;
-      index = 0;
-    }
-
-    public void addKeyframe(int time, float value) {
-      curve.Keys.Add(new CurveKey(time, value));
-      maxTime = time;
-      numPoints++;
+    public Animation(KeyFrames kf, bool loop = true) {
+      Loop = loop;
+      Frames = kf;
+      Index = 0;
     }
 
     public void reset() {
-      elapsedTime = 0;
-      index = 0;
-      smoothTangents();
+      ElapsedTime = 0;
+      Index = 0;
     }
 
-    public float update(GameTime gameTime, ref float value) {
+    public float Update(GameTime gameTime, ref float value) {
+      if (Frames == null) {
+        return 0;
+      }
+
       float lastValue = value;
-      value = curve.Evaluate(elapsedTime); // ref value represents actual point on curve
+      value = Frames.Evaluate(ElapsedTime, Index);
 
-      if (animationType == AnimationType.STEP) {
-        value = curve.Keys[index].Value;
-      }
-      if (float.IsNaN(value)) {
-        value = 0;
-      }
-
-      updateTime(gameTime);
+      UpdateTime(gameTime);
       return value - lastValue; // return delta 
     }
 
-    public float update(GameTime gameTime) {
-      float value = animationType == AnimationType.STEP ? curve.Keys[index].Value : curve.Evaluate(elapsedTime);
-
-      if (float.IsNaN(value)) {
-        value = 0;
-      }
-      updateTime(gameTime);
+    public float Update(GameTime gameTime) {
+      float value = Frames.Evaluate(ElapsedTime, Index);
+      UpdateTime(gameTime);
       return value;
     }
 
-    private void updateTime(GameTime gameTime) {
-      elapsedTime += gameTime.ElapsedGameTime.Milliseconds;
-      if (index < numPoints - 1 && elapsedTime >= curve.Keys[index + 1].Position) {
-        index++;
+    private void UpdateTime(GameTime gameTime) {
+      ElapsedTime += gameTime.ElapsedGameTime.Milliseconds;
+      if (Index < Frames.NumPoints - 1 && ElapsedTime >= Frames.Curve.Keys[Index + 1].Position) {
+        Index++;
       }
-      if (loop && elapsedTime > maxTime) {
-        elapsedTime = 0;
-        index = 0;
-      }
-    }
-
-    private void smoothTangents() {
-      CurveKey prev;
-      CurveKey current;
-      CurveKey next;
-      int prevIndex;
-      int nextIndex;
-      for (int i = 0; i < curve.Keys.Count; i++) {
-        prevIndex = i - 1;
-        if (prevIndex < 0) prevIndex = i;
-
-        nextIndex = i + 1;
-        if (nextIndex == curve.Keys.Count) nextIndex = i;
-
-        prev = curve.Keys[prevIndex];
-        next = curve.Keys[nextIndex];
-        current = curve.Keys[i];
-        SetCurveKeyTangent(ref prev, ref current, ref next);
-        curve.Keys[i] = current;
+      if (Loop && ElapsedTime > Frames.MaxTime) {
+        ElapsedTime = 0;
+        Index = 0;
       }
     }
 
-    private void SetCurveKeyTangent(ref CurveKey prev, ref CurveKey cur, ref CurveKey next) {
-      float dt = next.Position - prev.Position;
-      float dv = next.Value - prev.Value;
-      if (Math.Abs(dv) < float.Epsilon) {
-        cur.TangentIn = 0;
-        cur.TangentOut = 0;
+    public bool IsFinished() {
+      if (Loop) {
+        return false;
       }
-      else {
-        // The in and out tangents should be equal to the slope between the adjacent keys.
-        cur.TangentIn = dv * (cur.Position - prev.Position) / dt;
-        cur.TangentOut = dv * (next.Position - cur.Position) / dt;
-      }
+      return Index >= Frames.NumPoints - 1;
     }
   }
 }
